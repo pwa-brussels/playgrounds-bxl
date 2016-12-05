216f0173c50e23dcd85d4e6ce2479af5e6ac21a3
Simon Hampton <hotbelgo@gmail.com>
2016-12-05 12:20:18 +0100

    Move scripts to /src

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -37,33 +37,34 @@ import { updateCard } from './ui.js';
 
   document.getElementById('butRefresh').addEventListener('click', function() {
     // Refresh all of the forecasts
-    app.updateForecasts();
+    console.log('butRefresh');
+    // app.updateForecasts();
   });
 
-  document.getElementById('butAdd').addEventListener('click', function() {
-    // Open/show the add new city dialog
-    app.toggleAddDialog(true);
-  });
+  // document.getElementById('butAdd').addEventListener('click', function() {
+  //   // Open/show the add new city dialog
+  //   app.toggleAddDialog(true);
+  // });
 
-  document.getElementById('butAddCity').addEventListener('click', function() {
-    // Add the newly selected city
-    var select = document.getElementById('selectCityToAdd');
-    var selected = select.options[select.selectedIndex];
-    var key = selected.value;
-    var label = selected.textContent;
-    if (!app.selectedCities) {
-      app.selectedCities = [];
-    }
-    app.getForecast(key, label);
-    app.selectedCities.push({key: key, label: label});
-    app.saveSelectedCities();
-    app.toggleAddDialog(false);
-  });
+  // document.getElementById('butAddCity').addEventListener('click', function() {
+  //   // Add the newly selected city
+  //   var select = document.getElementById('selectCityToAdd');
+  //   var selected = select.options[select.selectedIndex];
+  //   var key = selected.value;
+  //   var label = selected.textContent;
+  //   if (!app.selectedCities) {
+  //     app.selectedCities = [];
+  //   }
+  //   app.getForecast(key, label);
+  //   app.selectedCities.push({key: key, label: label});
+  //   app.saveSelectedCities();
+  //   app.toggleAddDialog(false);
+  // });
 
-  document.getElementById('butAddCancel').addEventListener('click', function() {
-    // Close the add new city dialog
-    app.toggleAddDialog(false);
-  });
+  // document.getElementById('butAddCancel').addEventListener('click', function() {
+  //   // Close the add new city dialog
+  //   app.toggleAddDialog(false);
+  // });
 
 
   /*****************************************************************************
