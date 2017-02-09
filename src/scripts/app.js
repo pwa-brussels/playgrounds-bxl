f4ae7d8cbdec65184e7b49d4e9436eb040687886
Simon Hampton <hotbelgo@gmail.com>
2017-02-09 20:06:16 +0100

    Add basic payments request

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -47,31 +47,6 @@ let swRegistration;
     // app.updateForecasts();
   });
 
-  // document.getElementById('butAdd').addEventListener('click', function() {
-  //   // Open/show the add new city dialog
-  //   app.toggleAddDialog(true);
-  // });
-
-  // document.getElementById('butAddCity').addEventListener('click', function() {
-  //   // Add the newly selected city
-  //   var select = document.getElementById('selectCityToAdd');
-  //   var selected = select.options[select.selectedIndex];
-  //   var key = selected.value;
-  //   var label = selected.textContent;
-  //   if (!app.selectedCities) {
-  //     app.selectedCities = [];
-  //   }
-  //   app.getForecast(key, label);
-  //   app.selectedCities.push({key: key, label: label});
-  //   app.saveSelectedCities();
-  //   app.toggleAddDialog(false);
-  // });
-
-  // document.getElementById('butAddCancel').addEventListener('click', function() {
-  //   // Close the add new city dialog
-  //   app.toggleAddDialog(false);
-  // });
-
 
   /*****************************************************************************
    *
@@ -236,32 +211,19 @@ let swRegistration;
 
   // S E R V I C E   W O R K E R
 
-  if ('serviceWorker' in navigator && 'PushManager' in window) {
-    navigator.serviceWorker
-      .register('./service-worker.js')
-      .then((swReg) => {
-        swRegistration = swReg;
-        console.log('Service Worker Registered', swReg);
+  // if ('serviceWorker' in navigator && 'PushManager' in window) {
+  //   navigator.serviceWorker
+  //     .register('./service-worker.js')
+  //     .then((swReg) => {
+  //       swRegistration = swReg;
+  //       console.log('Service Worker Registered', swReg);
 
-        // Setup push notifications
-        initialiseSubs(swRegistration)
-          .then(sub => {
-            app.debug.textContent = JSON.stringify(sub);
-          });
-      });
-  }
+  //       // Setup push notifications
+  //       initialiseSubs(swRegistration)
+  //         .then(sub => {
+  //           app.debug.textContent = JSON.stringify(sub);
+  //         });
+  //     });
+  // }
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
 
